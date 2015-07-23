a1818d2f0f76a68af42b975adacd8c617184e7d4
Nick Groenen <nick@byte.nl>
2015-07-23 16:38:20 +0200

    Remove lsb_release reliance

--- bin/build1.sh
+++ bin/build1.sh
@@ -6,8 +6,9 @@ set -x
 apt-get update
 apt-get install --no-install-recommends -y ca-certificates curl
 
+source /etc/lsb-release
 curl --silent --show-error --location https://prosody.im/files/prosody-debian-packages.key | sudo apt-key add -
-echo deb http://packages.prosody.im/debian $(lsb_release -sc) main > /etc/apt/sources.list.d/prosody.list
+echo deb http://packages.prosody.im/debian $DISTRIB_CODENAME main > /etc/apt/sources.list.d/prosody.list
 
 apt-get update
 # lua-sec is needed for TLS
